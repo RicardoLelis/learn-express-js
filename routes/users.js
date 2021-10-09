7df520f60aea02641a015e1a32a14a4cb4bfa2dc
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-10-10 00:09:44 +0100

    Added  static files rendering and json parsing.

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(logger)
+
 router.get('/', (req, res) => {
     res.send("User List")
 })
 
 router.get('/new', (req, res) => {
-    res.send("User New Form")
+    // res.send("User New Form")
+    res.render("users/new", { firstName: "Test" })
 })
 
 router.post('/', (req, res) => {
-    res.send('Create user')
+    // res.send('Create user')
+    const isValid = true
+    if (isValid) {
+        users.push({ firstName: req.body.firstName })
+        res.redirect(`/users/${users.length - 1}`)
+    } else {
+        console.log("Error")
+        res.render('users/new', { firstName: req.body.firstName })
+    }
+    // console.log(req.body.firstName)
+    // res.send('Hi')
 })
 
 router.route("/:id").get((req, res) => {
@@ -30,5 +43,11 @@ router.param("id", (req, res, next, id) => {
     req.user = users[id]
     next()
 })
+
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 module.exports = router
