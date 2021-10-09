1eed44f03d5c396dcce99b7c63ecbb2e5d30e11b
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-10-09 22:48:23 +0100

    Added route dynamic parameter acceptance and also method parameter http verbs chaining.

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,30 @@ router.get('/new', (req, res) => {
     res.send("User New Form")
 })
 
+router.post('/', (req, res) => {
+    res.send('Create user')
+})
+
+router.route("/:id").get((req, res) => {
+    res.send(`Get user with ID: ${req.params.id}`)
+})
+    .put((req, res) => {
+        res.send(`Update user with ID: ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`Delete user with ID: ${req.params.id}`)
+    })
+// Same as below:
+// router.get('/:id', (req, res) => {
+//     res.send(`Get user with ID: ${req.params.id}`)
+// })
+
+// router.put('/:id', (req, res) => {
+//     res.send(`Update user with ID: ${req.params.id}`)
+// })
+
+// router.delete('/:id', (req, res) => {
+//     res.send(`Delete user with ID: ${req.params.id}`)
+// })
+
 module.exports = router
