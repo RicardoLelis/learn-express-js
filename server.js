9171981fdb8485341ff2c33e2a0756d35c1c8775
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-10-09 22:09:50 +0100

    Created our server connection and setup our first http route.

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Here')
+    res.send('Hi')
+})
+
+app.listen(3000)
